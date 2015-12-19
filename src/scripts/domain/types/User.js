edc3fb2dc810bb15047745ec7d1b67f987295982
dzdrazil <dzdrazil@gmail.com>
2015-12-18 22:52:06 -0600

    added some more docblocks

--- src/scripts/domain/types/User.js
+++ src/scripts/domain/types/User.js
@@ -1,9 +1,23 @@
 import t from 'tcomb';
 
+/**
+ * @class User
+ */
 export const User = t.struct({
+    /**
+     * @property username
+     * @type {String}
+     */
     username: t.String
 }, 'User');
 
+/**
+ * @class UserCredentials
+ */
 export const UserCredentials = User.extend({
+    /**
+     * @property password
+     * @type {String}
+     */
     password: t.String
 }, 'UserCredentials');
